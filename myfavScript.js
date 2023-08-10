cf1252fcd264b17de0f88a5ee8fe135c7fbe05de
jdmahto1998 <janardanmahto@gmail.com>
2023-08-10 21:22:38 +0530

    character page fix

--- myfavScript.js
+++ myfavScript.js
@@ -1,7 +1,3 @@
-let heroes = localStorage.getItem("fav");
-
-let favheroes = JSON.parse(heroes);
-
 
 var apikey= "3fac9c4542f5862f3d03f2f2eeaa889d";
 var privateKey ="6d868d05aec6c88c95814518acad060827d78516";
@@ -10,32 +6,6 @@ var baseUri = "https://gateway.marvel.com/v1/public/characters";
 var baseUriKeys = `ts=3&apikey=${apikey}&hash=${"dbd3fb9a458864dde50984bed11d004e"}`
 var contentElement = document.getElementById('content');
 
-
-
-function addToFavorite(id){
-    // event.preventDefault();
-    console.log("in add to fav");
-    id= parseInt(id);
-    let heroes = localStorage.getItem("fav");
-    let favheroes=JSON.parse(heroes);
-    if(favheroes==null)
-    favheroes=[id];
-    else
-    favheroes.push(id);
-    localStorage.setItem("fav",JSON.stringify(favheroes));
-
-    get();
-}
-
-function checkForfav(id){
-    let heroes = localStorage.getItem("fav");
-    let favheroes = JSON.parse(heroes);
-    if(favheroes==null)
-    return false;
-    if(favheroes.includes(id))
-    return true;
-    return false;
-}
 function removeToFav(id){
 
     console.log("in remove from fav");
@@ -46,7 +16,17 @@ function removeToFav(id){
     get();   
 }
 
+function handler(event){
+    console.log(event);
+   
+    removeToFav(event.target.attributes[0].nodeValue);
+}
+
+
  function get(){
+    let heroes = localStorage.getItem("fav");
+
+let favheroes = JSON.parse(heroes);
 
     var htmlElements="";
 
@@ -64,7 +44,7 @@ favheroes.forEach(async (hero)=>{
            <p> ${res.description==""?"no description found!":res.description}</p>
            </a>
           <div class="favorite">
-           <button heroId=${res.id} onclick="handler(event)">${checkForfav(res.id)?'remove from favorite':'add to favorite'}</button>
+           <button heroId=${res.id} onclick="handler(event)">remove from favorite</button>
            </div>
        </div>`
        
